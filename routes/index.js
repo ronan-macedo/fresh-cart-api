95bb6edeab2516dad36b7d88dfe72224822bc64b
KCMinchakpu <min22002@byui.edu>
2024-02-07 03:33:05 +0100

    updating repo

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,11 @@ router.get('/', (req, res) => {
     res.send('Hello Team Wednesday 1pm!');
 });
 
+router.use('/membership', require('./membership'));
+router.use('/products', require('./products'));
+router.use('/customers', require('./customers'));
+router.use('/sales', require('./sales'));
+router.use('/identities', require('./identities'));
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,6 @@ mongodb.initDb((err) => {
       console.log(err);
     } else {
       app.listen(port);
-      console.log(`Connected to Database and listening on ${port}`);
+      console.log(`Database Listening and Node Running on port ${port}`);
     }
   });

4506dad163fc361330d31435f263dfcd8aa6963f
KCMinchakpu <min22002@byui.edu>
2024-02-07 03:15:30 +0100

    updating repo

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const mongodb = require('./data/database');
 
 const port = process.env.PORT || 5500;
 
 app.use('/', require('./routes'));
 
-app.listen(port);
-    console.log(`Running on ${port}`);
+mongodb.initDb((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to Database and listening on ${port}`);
+    }
+  });
